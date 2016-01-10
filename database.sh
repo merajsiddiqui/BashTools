3da29c97c8a3ba213d8453d8d34e8d906e72b750
Meraj Ahmad Siddiqui <merajsiddiqui@outlook.com>
2016-01-10 15:04:56 +0530

    Started the Work

---
+++ database.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##########################################################################
+#  Sscript 		: Database											##
+#																		##
+#  Author  		: Meraj Ahmad Siddiqui <merajsiddiqui@outlook.com>		##
+#																		##
+#  Date	   		: 10 January 2016, Sunday								##
+#																		##
+#  Description	: This is a Databse page which containsfunctions to		## 
+#				  install diffrent databases like mysql, postgre sql 	##
+#				  or mongodb and NoSQL, It also contains unction for	##
+#				  completely uninstalling them and their bacup			##
+#																		##
+#  Note 		: In this page only function calls are made hence if 	##
+#				  you want to perform limited operation all you need 	##
+#			      to do is comment out that function  by '#' sign.		##	
+##########################################################################
+
+
+#
+#This funcion is to installl mysql databases
+#
+
+function InstallMySQL(){
+	#Update Your Repository First
+	apt-get install
+}
---
+++ index.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+##########################################################################
+#  Sscript 		: Index.sh												##
+#																		##
+#  Author  		: Meraj Ahmad Siddiqui <merajsiddiqui@outlook.com>		##
+#																		##
+#  Date	   		: 10 January 2016, Sunday								##
+#																		##
+#  Description	: This is a Bash Framework Index page which excutes		## 
+#				  lots of function to perform Diffrent Type of work 	##
+#				  and perform multiple operation to basically manage	##
+#				  servers, health check of system and many more			##
+#																		##
+#  Note 		: In this page only function calls are made hence if 	##
+#				  you want to perform limited operation all you need 	##
+#			      to do is comment out that function  by '#' sign.		##	
+##########################################################################
+
